982e31d59d6aa62c791a4ca81e1c2f974eadbda2
Konstantin Voykov <koykov0@gmail.com>
2021-07-25 15:21:11 +0300

    Fix unsafe.Pointer misuse problem.

--- entry.go
+++ entry.go
@@ -1,14 +1,12 @@
 package cbytecache
 
-import "unsafe"
-
 type entry struct {
 	offset uint32
 	length uint16
 	expire uint32 // overflows at 2106-02-07 06:28:15
-	aidptr uintptr
+	aidptr *uint32
 }
 
 func (e entry) arenaID() uint32 {
-	return *(*uint32)(unsafe.Pointer(e.aidptr))
+	return *e.aidptr
 }
