4117beaecf4fc9fddc94ec25b4ab32da59d5690d
Konstantin Voykov <koykov0@gmail.com>
2021-07-29 23:26:43 +0300

    Default cache config helpers.

--- config.go
+++ config.go
@@ -19,3 +19,20 @@ func (c *Config) Copy() *Config {
 	cpy := *c
 	return &cpy
 }
+
+func DefaultConfig(expire time.Duration, hashFn func(string) uint64) *Config {
+	c := Config{
+		HashFn: hashFn,
+		Shards: 1024,
+		Expire: expire,
+	}
+	return &c
+}
+
+func DefaultConfigWS(expire time.Duration, hashFn func(string) uint64, maxSize MemorySize) *Config {
+	c := DefaultConfig(expire, hashFn)
+	c.MaxSize = maxSize
+	return c
+}
+
+var _ = DefaultConfigWS
