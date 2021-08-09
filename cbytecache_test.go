8cd9bd56e726858c10900ab5f940db48dcce4d53
Konstantin Voykov <koykov0@gmail.com>
2021-08-09 22:36:40 +0300

    Test issues: cover single IO.

--- cbytecache_test.go
+++ cbytecache_test.go
@@ -1,5 +1,46 @@
 package cbytecache
 
+import (
+	"bytes"
+	"testing"
+	"time"
+
+	"github.com/koykov/fastconv"
+)
+
+var (
+	dataPool = [][]byte{
+		[]byte(`{"firstName":"John","lastName":"Smith","isAlive":true,"age":27,"address":{"streetAddress":"21 2nd Street","city":"New York","state":"NY","postalCode":"10021-3100"},"phoneNumbers":[{"type":"home","number":"212 555-1234"},{"type":"office","number":"646 555-4567"},{"type":"mobile","number":"123 456-7890"}],"children":[],"spouse":null}`),
+		[]byte(`{"$schema":"http://json-schema.org/schema#","title":"Product","type":"object","required":["id","name","price"],"properties":{"id":{"type":"number","description":"Product identifier"},"name":{"type":"string","description":"Name of the product"},"price":{"type":"number","minimum":0},"tags":{"type":"array","items":{"type":"string"}},"stock":{"type":"object","properties":{"warehouse":{"type":"number"},"retail":{"type":"number"}}}}}`),
+		[]byte(`{"id":1,"name":"Foo","price":123,"tags":["Bar","Eek"],"stock":{"warehouse":300,"retail":20}}`),
+		[]byte(`{"first name":"John","last name":"Smith","age":25,"address":{"street address":"21 2nd Street","city":"New York","state":"NY","postal code":"10021"},"phone numbers":[{"type":"home","number":"212 555-1234"},{"type":"fax","number":"646 555-4567"}],"sex":{"type":"male"}}`),
+		[]byte(`{"fruit":"Apple","size":"Large","color":"Red"}`),
+		[]byte(`{"quiz":{"sport":{"q1":{"question":"Which one is correct team name in NBA?","options":["New York Bulls","Los Angeles Kings","Golden State Warriros","Huston Rocket"],"answer":"Huston Rocket"}},"maths":{"q1":{"question":"5 + 7 = ?","options":["10","11","12","13"],"answer":"12"},"q2":{"question":"12 - 8 = ?","options":["1","2","3","4"],"answer":"4"}}}}`),
+	}
+)
+
+func assertBytes(t testing.TB, a, b []byte) {
+	if !bytes.Equal(a, b) {
+		t.Errorf("bytes equal fail:\nneed: %s\ngot:  %s", a, b)
+	}
+}
+
+func TestCacheIOSingle(t *testing.T) {
+	conf := DefaultConfig(time.Minute, fastconv.Fnv64aString)
+	cache, err := NewCByteCache(conf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err = cache.Set("key135", dataPool[0]); err != nil {
+		t.Error(err)
+	}
+	var dst []byte
+	if dst, err = cache.Get("key135"); err != nil {
+		t.Error(err)
+	}
+	assertBytes(t, dataPool[0], dst)
+}
+
 // import (
 // 	"math/rand"
 // 	"strings"
