3f01a8de690924f5547773852ce630219885e347
Konstantin Voykov <koykov0@gmail.com>
2021-12-20 22:35:32 +0200

    Code comment issues.

--- clock.go
+++ clock.go
@@ -6,15 +6,25 @@ import (
 	"time"
 )
 
+// Clock describes timer helper with testing features (like Jump).
 type Clock interface {
+	// Start the clock.
 	Start()
+	// Stop the clock.
 	Stop()
+	// Active checks if close is started.
 	Active() bool
+	// Now returns current time considering jumps.
 	Now() time.Time
+	// Jump performs time travel to delta (maybe negative). Now and scheduled jobs considers jumps.
 	Jump(delta time.Duration)
+	// Schedule registers fn to call at every d.
 	Schedule(d time.Duration, fn func())
 }
 
+// NativeClock is a primitive clock based on time package.
+//
+// Jump doesn't work in that implementation.
 type NativeClock struct {
 	mux    sync.Mutex
 	cancel []context.CancelFunc
