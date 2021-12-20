50a2135ed42d5edc54d4bda1413150be7fdafe9f
Konstantin Voykov <koykov0@gmail.com>
2021-12-20 22:27:48 +0200

    Code comment issues.

--- arena.go
+++ arena.go
@@ -6,23 +6,31 @@ import (
 	"github.com/koykov/cbyte"
 )
 
+// Memory arena implementation.
 type arena struct {
 	id uint32
 	h  reflect.SliceHeader
 }
 
+// Create and alloc space for new arena.
 func allocArena(id uint32) *arena {
 	a := &arena{id: id}
 	a.h = cbyte.InitHeader(0, int(ArenaSize))
 	return a
 }
 
+// Write b to arena.
+//
+// Caution! No bounds check control. External code must guarantee the safety.
 func (a *arena) write(b []byte) (n int) {
 	n = cbyte.Memcpy(uint64(a.h.Data), uint64(a.h.Len), b)
 	a.h.Len += n
 	return
 }
 
+// Read bytes from arena using offset and length.
+//
+// Caution! No bounds check control. External code must guarantee the safety.
 func (a *arena) read(offset, length uint32) []byte {
 	h := reflect.SliceHeader{
 		Data: a.h.Data + uintptr(offset),
@@ -32,14 +40,19 @@ func (a *arena) read(offset, length uint32) []byte {
 	return cbyte.Bytes(h)
 }
 
+// Get free space offset (or length of using space).
 func (a arena) offset() uint32 {
 	return uint32(a.h.Len)
 }
 
+// Get length of free space in arena.
 func (a arena) rest() uint32 {
 	return uint32(a.h.Cap - a.h.Len)
 }
 
+// Release memory arena.
+//
+// Arena object doesn't destroy. Using it afterward is unsafe.
 func (a *arena) release() {
 	cbyte.ReleaseHeader(a.h)
 }
