6964415d56d3afb4b3cd58aaeab59824a59693e7
Konstantin Voykov <koykov0@gmail.com>
2022-10-30 04:23:28 +0200

    Call expire listener issue.

--- bucket.go
+++ bucket.go
@@ -337,6 +337,10 @@ func (b *bucket) bulkEvict() error {
 
 	arenaID := b.entry[z-1].arenaID()
 
+	if b.config.ExpireListener != nil {
+		b.expireRange(z)
+	}
+
 	var wg sync.WaitGroup
 
 	wg.Add(1)
@@ -435,6 +439,42 @@ func (b *bucket) recycleArena(arenaID uint32) {
 			b.arena[i].h.Len = 0
 		}
 	}
+}
+
+func (b *bucket) expireRange(z int) {
+	el := b.elen()
+	if z < 256 {
+		_ = b.entry[el-1]
+		for i := 0; i < z; i++ {
+			b.expire(&b.entry[i])
+		}
+	} else {
+		z8 := z - z%8
+		_ = b.entry[el-1]
+		for i := 0; i < z8; i += 8 {
+			b.expire(&b.entry[i])
+			b.expire(&b.entry[i+1])
+			b.expire(&b.entry[i+2])
+			b.expire(&b.entry[i+3])
+			b.expire(&b.entry[i+4])
+			b.expire(&b.entry[i+5])
+			b.expire(&b.entry[i+6])
+			b.expire(&b.entry[i+7])
+		}
+		for i := z8; i < z; i++ {
+			b.expire(&b.entry[i])
+		}
+	}
+}
+
+func (b *bucket) expire(e *entry) {
+	b.buf.ResetLen()
+	_ = b.buf.GrowLen(int(e.length))
+	key, body, err := b.getLF(b.buf.Bytes(), e, dummyMetrics)
+	if err != nil {
+		return
+	}
+	_ = b.config.ExpireListener.Listen(key, body)
 }
 
 func (b *bucket) evictRange(z int) {
