527f3596a54054465ae628c61d4a5904c02d9489
Konstantin Voykov <koykov0@gmail.com>
2022-11-04 00:01:11 +0200

    Update index after bulk eviction.

--- evict.go
+++ evict.go
@@ -25,8 +25,35 @@ func (b *bucket) evictRange(z int) {
 		}
 	}
 
+	// Move non-expired entries to the start of entries list.
 	copy(b.entry, b.entry[z:])
 	b.entry = b.entry[:el-uint32(z)]
+
+	// Update index.
+	if el = b.elen(); el == 0 {
+		return
+	}
+	if el < 256 {
+		_ = b.entry[el-1]
+		for i := uint32(0); i < el; i++ {
+			b.index[b.entry[i].hash] = i
+		}
+	} else {
+		el8 := el - el%8
+		for i := uint32(0); i < el8; i += 8 {
+			b.index[b.entry[i].hash] = i
+			b.index[b.entry[i+1].hash] = i + 1
+			b.index[b.entry[i+2].hash] = i + 2
+			b.index[b.entry[i+3].hash] = i + 3
+			b.index[b.entry[i+4].hash] = i + 4
+			b.index[b.entry[i+5].hash] = i + 5
+			b.index[b.entry[i+6].hash] = i + 6
+			b.index[b.entry[i+7].hash] = i + 7
+		}
+		for i := el8; i < el; i++ {
+			b.index[b.entry[i].hash] = i
+		}
+	}
 }
 
 func (b *bucket) evict(e *entry) {
