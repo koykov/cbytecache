330ad74cff52cebca0928823aaaa3c6d56f7ef06
Konstantin Voykov <koykov0@gmail.com>
2022-12-03 20:47:14 +0200

    Rollback arena metrics.

--- dummy.go
+++ dummy.go
@@ -4,20 +4,19 @@ import "time"
 
 type DummyMetrics struct{}
 
-func (DummyMetrics) Alloc(_ string)                       {}
-func (DummyMetrics) Fill(_ string)                        {}
-func (DummyMetrics) Reset(_ string)                       {}
-func (DummyMetrics) Release(_ string)                     {}
-func (DummyMetrics) ArenaMap(_ string, _, _, _, _ uint32) {}
-func (DummyMetrics) Set(_ string, _ time.Duration)        {}
-func (DummyMetrics) Hit(_ string, _ time.Duration)        {}
-func (DummyMetrics) Evict(_ string)                       {}
-func (DummyMetrics) Miss(_ string)                        {}
-func (DummyMetrics) Expire(_ string)                      {}
-func (DummyMetrics) Corrupt(_ string)                     {}
-func (DummyMetrics) Collision(_ string)                   {}
-func (DummyMetrics) NoSpace(_ string)                     {}
-func (DummyMetrics) Dump()                                {}
-func (DummyMetrics) Load()                                {}
+func (DummyMetrics) Alloc(_ string, _ uint32)      {}
+func (DummyMetrics) Fill(_ string, _ uint32)       {}
+func (DummyMetrics) Reset(_ string, _ uint32)      {}
+func (DummyMetrics) Release(_ string, _ uint32)    {}
+func (DummyMetrics) Set(_ string, _ time.Duration) {}
+func (DummyMetrics) Hit(_ string, _ time.Duration) {}
+func (DummyMetrics) Evict(_ string)                {}
+func (DummyMetrics) Miss(_ string)                 {}
+func (DummyMetrics) Expire(_ string)               {}
+func (DummyMetrics) Corrupt(_ string)              {}
+func (DummyMetrics) Collision(_ string)            {}
+func (DummyMetrics) NoSpace(_ string)              {}
+func (DummyMetrics) Dump()                         {}
+func (DummyMetrics) Load()                         {}
 
 var dummyMetrics = DummyMetrics{}
--- metrics.go
+++ metrics.go
@@ -3,11 +3,10 @@ package cbytecache
 import "time"
 
 type MetricsWriter interface {
-	Alloc(bucket string)
-	Fill(bucket string)
-	Reset(bucket string)
-	Release(bucket string)
-	ArenaMap(bucket string, total, used, free, size uint32)
+	Alloc(bucket string, size uint32)
+	Fill(bucket string, size uint32)
+	Reset(bucket string, size uint32)
+	Release(bucket string, size uint32)
 	Set(bucket string, dur time.Duration)
 	Hit(bucket string, dur time.Duration)
 	Evict(bucket string)
