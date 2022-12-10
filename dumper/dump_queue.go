31f97273bdada971d739deb79efd4588f9fb6fa6
Konstantin Voykov <koykov0@gmail.com>
2022-12-10 14:27:17 +0200

    Comment issues: dump queue.

--- dumper/dump_queue.go
+++ dumper/dump_queue.go
@@ -4,10 +4,12 @@ import (
 	"github.com/koykov/cbytecache"
 )
 
+// DumpQueue is a DumpWriter wrapper over enqueuer.
 type DumpQueue struct {
 	Enqueuer cbytecache.Enqueuer
 }
 
+// NewQueue makes new DumpQueue instance with given enqueuer.
 func NewQueue(enq cbytecache.Enqueuer) (*DumpQueue, error) {
 	if enq == nil {
 		return nil, cbytecache.ErrNoEnqueuer
@@ -16,12 +18,12 @@ func NewQueue(enq cbytecache.Enqueuer) (*DumpQueue, error) {
 	return &q, nil
 }
 
-func (q *DumpQueue) Write(entry cbytecache.Entry) error {
+func (q *DumpQueue) Write(entry cbytecache.Entry) (int, error) {
 	if q.Enqueuer == nil {
-		return cbytecache.ErrNoEnqueuer
+		return 0, cbytecache.ErrNoEnqueuer
 	}
 	cpy := entry.Copy()
-	return q.Enqueuer.Enqueue(cpy)
+	return cpy.Size(), q.Enqueuer.Enqueue(cpy)
 }
 
 func (q *DumpQueue) Flush() error {
